aba64ec483f9042306ae75c12eddb2112e065484
MKhalusova <kafooster@gmail.com>
2021-09-23 09:35:14 -0400

    Adding cml workflow. Changed model to polynomial

--- train.py
+++ train.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import PolynomialFeatures
 
 df = pd.read_csv("data/data_processed.csv")
 
@@ -26,13 +27,15 @@ y_scaler = StandardScaler()
 Y_train = y_scaler.fit_transform(np.array(Y_train).reshape(-1, 1)).squeeze()
 Y_test = y_scaler.transform(np.array(Y_test).reshape(-1, 1)).squeeze()
 
-# Train the simplest Linear model
-lrm = LinearRegression().fit(X_train, Y_train)
+# Train polynomial model
+poly_reg = PolynomialFeatures(degree=2)
+X_poly = poly_reg.fit_transform(X_train)
 
-# Capture the metrics
-r_2 = lrm.score(X_test, Y_test)
-Y_pred = lrm.predict(X_test)
-rmse = np.sqrt(mean_squared_error(Y_test, Y_pred))
+lin_poly = LinearRegression().fit(X_poly, Y_train)
+r_2 = lin_poly.score(poly_reg.transform(X_test), Y_test)
+
+Y_pred2= lin_poly.predict(poly_reg.transform(X_test))
+rmse = np.sqrt(mean_squared_error(Y_test, Y_pred2))
 
 # Record the metrics to the metrics.json file
 with open("metrics.json", 'w') as outfile:
@@ -43,4 +46,4 @@ models_dir = Path('model')
 models_dir.mkdir(exist_ok=True)
 model_path = models_dir / "model.pkl"
 with open(model_path, 'wb') as fp:
-    pickle.dump(lrm, fp)
+    pickle.dump(lin_poly, fp)
